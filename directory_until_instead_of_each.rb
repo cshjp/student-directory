183b23bfee24e332f691338c2d631d610d05a421
Charlie Parker <cshjparker@gmx.co.uk>
2023-04-04 10:59:22 +0000

    directory that uses the until method loop instead of each method

---
+++ directory_until_instead_of_each.rb
@@ -0,0 +1,33 @@
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    students = []
+    user_input = gets.chomp
+    while !user_input.empty? do
+        students << {name: user_input, cohort: :november}
+        puts "We now have #{students.count} students"
+        user_input = gets.chomp
+    end
+    students
+end
+
+def print_header
+puts "Students at Villains Academy"
+puts "----------------------------"
+end
+
+def print(names)
+    until names.empty?
+        name = names.pop
+        puts "#{name[:name]}, (#{name[:cohort]} cohort)"
+    end
+end
+
+def print_footer(names)
+puts "Overall we have #{names.count} great students!"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
