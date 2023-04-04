2ed31ae0256c711beaed8da3e869868353e162b7
Charlie Parker <cshjparker@gmx.co.uk>
2023-04-04 10:28:55 +0000

    directory that only prints out student names that aren't longer than 12 letters

---
+++ directory_less_than_12.rb
@@ -0,0 +1,34 @@
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    students = []
+    user_input = gets.chomp
+    while !user_input.empty? do
+        students << {name: user_input, cohort: :november}
+        puts "We now have #{students.count} students"
+        user_input = gets.chomp
+    end
+    students
+end
+
+def print_header
+puts "Students at Villains Academy"
+puts "----------------------------"
+end
+
+def print(names)
+    names.each do |name|
+      if name[:name].length < 12
+        puts "#{name[:name]}, (#{name[:cohort]} cohort)"
+      end
+    end
+end
+
+def print_footer(names)
+puts "Overall we have #{names.count} great students!"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
