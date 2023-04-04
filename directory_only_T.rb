924b8ca00bc749f8afc9d7d7096d37d8f53f317e
Charlie Parker <cshjparker@gmx.co.uk>
2023-04-04 10:25:44 +0000

    directory that only prints out student names that start with T (changed in source)

---
+++ directory_only_T.rb
@@ -0,0 +1,32 @@
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    students = []
+    user_input = gets.chomp
+    while !user_input.empty? do
+        students << {name: user_input, cohort: :november}
+        puts "We now have #{students.count} students"
+        user_input = gets.chomp
+    end
+    students
+end
+
+def print_header
+puts "Students at Villains Academy"
+puts "----------------------------"
+end
+
+def print(names)
+    names.each do |name|
+        puts "#{name[:name.starts_with?("T")]}, (#{name[:cohort]} cohort)"
+    end
+end
+
+def print_footer(names)
+puts "Overall we have #{names.count} great students!"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
