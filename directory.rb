96b98dda12bf3dbc1d69d441b33c7477c06aea49
Charlie Parker <cshjp@MASTUDENT-Charlie-Parker-C02G4FQVQ6L4.local>
2023-04-07 16:23:16 +0100

    added interative menu thats also refactored down

--- directory.rb
+++ directory.rb
@@ -1,18 +1,18 @@
+@students = []
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    students = []
     user_input = gets.chomp
     while !user_input.empty? do
-        students << {name: user_input, cohort: :november}
-        if students.count == 1
-          puts "We now have #{students.count} student"
+        @students << {name: user_input, cohort: :november}
+        if @students.count == 1
+          puts "We now have #{@students.count} student"
         else
-          puts "We now have #{students.count} students"
+          puts "We now have #{@students.count} students"
         end
         user_input = gets.chomp
     end
-    students
 end
 
 def print_header
@@ -20,22 +20,50 @@ puts "Students at Villains Academy"
 puts "----------------------------"
 end
 
-def print(names)
-    names.each_with_index do |name, index|
-        puts "#{index + 1}. #{name[:name]}, (#{name[:cohort]} cohort)"
+def print_student_list
+    @students.each_with_index do |student, index|
+        puts "#{index + 1}. #{student[:name]}, (#{student[:cohort]} cohort)"
     end
 end
 
-def print_footer(names)
-if names.count == 1
-puts "Overall we have #{names.count} great student!"
+def print_footer
+if @students.count == 1
+puts "Overall we have #{@students.count} great student!"
 else
-puts "Overall we have #{names.count} great students!"
+puts "Overall we have #{@students.count} great students!"
 end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
-test
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "Try Again, I didn't understand"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
